8f966fa5ecbf4d0ade06540c74d10e7760fd1e43
Igor <55404866+CepbluKot@users.noreply.github.com>
2021-11-27 21:59:11 +0300

    Update screens.py

--- screens.py
+++ screens.py
@@ -1,31 +1,61 @@
-
 from logging import root
+from kivy.core import text
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition
 from kivymd.app import MDApp
 from kivy.core.window import Window
-
+from kivymd.uix.datatables import MDDataTable
+from kivy.uix.button import Button
+from kivymd.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.metrics import dp
 
 Window.size = (1920, 1080)
 
 
 class LoginScreen(Screen):
-
     pass
 
 
 class NavScreen(Screen):
-    pass
-
+      pass
+    
 
 class WindowManager(ScreenManager):
-    pass
+    
+    def update_data(self):
+        smth = self.ids.text_one.text
+        lin = self.ids.syda
+        print(smth)
+        
+        det = BoxLayout()
 
+        table = MDDataTable(
+            check=True,
+            size_hint=(0.5, 0.6),
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+            column_data=[
+                ('fir', dp(30)),
+                ('sec', dp(30)),
+                ('thi', dp(30))
+            ],
+            row_data=[
+                ('smb', '123', 'aaa'),
+                ('smb 2', '345', 'bbb'),
+                ('smb last', '999', 'ccc')
+            ]
+        )
+
+        lin.add_widget(det)
+        
+        
+        det.add_widget(table)
 
 class TestApp(MDApp):
 
     def build(self):
         kv = Builder.load_file('scrn.kv')
+        
 
         return kv
 
@@ -38,5 +68,7 @@ class TestApp(MDApp):
         self.root.transition = RiseInTransition()
         self.root.current = 'NavScreen'
 
+
+        
 
 TestApp().run()
